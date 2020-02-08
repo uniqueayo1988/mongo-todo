06802e0ae9378f42d4f9addb81507a6b2c561989
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2020-02-08 22:36:50 +0100

    Add private middleware

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
 // User Model
 var UserSchema = new mongoose.Schema({
@@ -33,10 +34,44 @@ var UserSchema = new mongoose.Schema({
   }]
 })
 
-UserSchema.methods.generateAuthToken = function () {
+UserSchema.methods.toJSON = function () {
   var user = this
+  var userObject = user.toObject()
+
+  return _.pick(userObject, ['_id', 'email'])
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this   // caalling the instace
   var access = 'auth'
-  var token = 
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString()
+
+  user.tokens = user.tokens.concat([{access, token}])
+
+  return user.save().then(() => {
+    return token
+  })
+}
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this   // calling the Model
+  var decoded
+
+  try {
+    decoded = jwt.verify(token, 'abc123')
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject()
+    // })
+    // return Promise.reject()
+    return Promise.reject('Error')
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  })
 }
  
 var User = mongoose.model('User', UserSchema)
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectId} = require('mongodb')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express()
 const port = process.env.PORT || 3000
@@ -98,12 +99,18 @@ app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password'])
   var user = new User(body)
 
-  user.save().then(user => {
-    res.status(201).send(user)
-  }, (e) => {
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch(e => {
     // res.status(400).send({message: 'Unable to create document'})
     res.status(400).send(e)
   })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 })
 
 app.listen(port, () => {
