c1c2193702a02dfa76f8633f3c15651104bda419
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2020-02-23 17:06:12 +0100

    Seed data

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,22 +1,41 @@
 const SHA256 = require("crypto-js/sha256")
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-  id: 10
-}
+// example 0
+var password = '123abc!'
 
-var token = jwt.sign(data, '123abc')
-console.log(token)
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash, '...hash')
+//   })
+// })
 
-var decoded = jwt.verify(token, '123abc')
-console.log(decoded)
+var hashedPassword = '$2a$10$u.9PIxCH4SIdIS18goWTdOUowDH7ry9B5p/PmOk4mZmaFU6B3PJxO'
 
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res, '...response')
+})
+
+// // example 1
+// var data = {
+//   id: 10
+// }
+
+// var token = jwt.sign(data, '123abc')
+// console.log(token)
+
+// var decoded = jwt.verify(token, '123abc')
+// console.log(decoded)
+
+// // example 2
 // var message = 'I am user number 3'
 // var hash = SHA256(message).toString()
 
 // console.log(`Message: ${message}`)
 // console.log(`Hash: ${hash}`)
 
+// // example 3
 // var data = {
 //   id: 4
 // }
